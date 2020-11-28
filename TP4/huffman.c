84f68aef8638b5f83f6ea396db8601933dcbddcc
d3vyce <nicolas.sudres@hotmail.fr>
2020-11-28 11:58:17 +0100

    Création fichier huffman.c

---
+++ TP4/huffman.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct noeud {
+    unsigned char c;
+    int occurence;
+    int code;
+    int bits;
+    struct noeud *gauche, *droit;
+}noeud;
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
